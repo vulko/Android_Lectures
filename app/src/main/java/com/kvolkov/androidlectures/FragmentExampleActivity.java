2d36cb8e945864c826f89b5811edde8b5d9a954b
Kirill Volkov <kirill.volkov@honeywell.com>
2020-08-24 20:25:29 +0300

    Add fragment example.

---
+++ app/src/main/java/com/kvolkov/androidlectures/FragmentExampleActivity.java
@@ -0,0 +1,50 @@
+package com.kvolkov.androidlectures;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class FragmentExampleActivity extends FragmentActivity {
+
+    private int mCounter = 1;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_activity_example);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.fragment_container, ExampleFragment.newInstance("Fragment " + mCounter, ""));
+        transaction.addToBackStack("Fragment " + mCounter);
+        mCounter++;
+        transaction.commit();
+
+        ((Button) findViewById(R.id.add_fragment_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNextFragment();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        mCounter--;
+        if (!getSupportFragmentManager().popBackStackImmediate()) {
+            super.onBackPressed();
+        }
+    }
+
+    private void showNextFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.fragment_container, ExampleFragment.newInstance("Fragment " + mCounter, ""));
+        transaction.addToBackStack("Fragment " + mCounter);
+        mCounter++;
+        transaction.commit();
+    }
+
+}
